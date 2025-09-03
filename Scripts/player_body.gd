b74fcd1ab943122f70b41379aa1fc1b200b7db07
Squid <sagewolv@gmail.com>
2025-09-03 18:43:44 +0000

    i basicaly did nothing. i forgor how to run a for loop through a tilemap layer

--- Scripts/player_body.gd
+++ Scripts/player_body.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @export var moveSpeed := 1
-@onready var ground: TileMapLayer = $".."
+@onready var ground: TileMapLayer = $"."
+@onready var player: Node2D = $Player
 var astar = AStarGrid2D.new()
 var idPath := []
 var used_idPath := []
@@ -8,13 +9,17 @@ var used_idPath := []
 ### _Functions #################################################################
 func _ready() -> void:
 	setUpAstarGrid()
+	#for x in ground.get_used_rect():
+		#for y in ground.get_used_rect():
+			
+	print(ground.get_used_rect())
 	
 func _input(event: InputEvent) -> void:
 	### if the player hasn't clicked, an input isnt ran to save processing power
 	if event.is_action_pressed("Mouse Left") == false:
 		return
 		
-	var Ipositipon := ground.local_to_map(global_position) ## the id location of the player on the tile map
+	var Ipositipon := ground.local_to_map(player.global_position) ## the id location of the player on the tile map
 	var mouseIposition := ground.local_to_map(get_global_mouse_position()) ## same for the mouse
 	
 	### idPath is an array of all id positions of the squares needed
@@ -31,9 +36,9 @@ func _physics_process(delta):
 		### defines the position for the player to move towrds as the first element of the used path
 		var targetPostion = ground.map_to_local(used_idPath.front())
 		### moves the player towards target
-		global_position = global_position.move_toward(targetPostion, moveSpeed)
+		player.global_position = player.global_position.move_toward(targetPostion, moveSpeed)
 		### repeats the code above as popping the front element creates a new target
-		if global_position == targetPostion:
+		if player.global_position == targetPostion:
 			used_idPath.pop_front()
 			
 		
